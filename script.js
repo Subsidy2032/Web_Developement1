4a2981a768f851eaa0fc1fe4b187810a69c26177
Subsidy2032 <noam133m@gmail.com>
2024-04-30 09:30:10 +0300

    Final Changes

--- script.js
+++ script.js
@@ -7,68 +7,68 @@ function validateInput() {
   // Getting the password input and calling the function to validate the password
   let pw = document.forms["signupForm"]["psw"].value;
   let pwRepeat = document.forms["signupForm"]["psw-repeat"].value;
-
   let passValid = validatePw(pw, pwRepeat); // Saving if there is a problem with the password
 
-  // Getting the email input
-  let email = document.forms["signupForm"]["email"].value.trim();
-
-  let emailNotEmpty = true; // Variable to save if the email field is empty
-
-  // Checks if the email field is empty, sets the border color the red if it is, sets it to default otherwise
-  if(email === "") {
-    document.getElementById("email").style.border = '1px solid red';
-    emailNotEmpty = false;
-  } else {
-    document.getElementById("email").style.border = '';
-  }
+  // Getting the email input and calling the function to validate the email
+  let email = document.forms["signupForm"]["email"].value.trim(); // Getting the email input
+  let emailValid = validateEmail(email);
   
-  return passValid && emailNotEmpty; // Returns true if the entire signup input is valid, false otherwise
+  return passValid && emailValid; // Returns true if the entire signup input is valid, false otherwise
 }
 
 // A function to check if the password fields are valid
 function validatePw(pw, pwRepeat) {
-  let pwNotEmpty = true;
-  let pwRepeatNotEmpty = true;
+  let passValid = true; // To track if the password is valid
 
-  // Checks if the password field is empty, sets the border color the red if it is, sets it to default otherwise
+  // Resets the error messages and borders style
+  document.getElementById("repeatPassMessage").innerHTML = "";
+  document.getElementById("passMessage").innerHTML = "";
+  document.getElementById("pswRepeat").style.border = '';
+  document.getElementById("psw").style.border = '';
+
+   // Checks if the repeat password field is empty, sets the border color the red if it is
+   if(pwRepeat === "") {
+    document.getElementById("pswRepeat").style.border = '1px solid red';
+    passValid = false;
+  }
+
+  // Checks if the password field is empty, sets the border color the red if it is
   if(pw === "") {
     document.getElementById("psw").style.border = '1px solid red';
-    pwNotEmpty = false;
-    document.getElementById("message").innerHTML = "";
-  } else {
-    document.getElementById("psw").style.border = '';
-  }
-
-  // Checks if the repeat password field is empty, sets the border color the red if it is, sets it to default otherwise
-  if(pwRepeat === "") {
-    document.getElementById("pswRepeat").style.border = '1px solid red';
-    pwRepeatNotEmpty = false;
-  } else {
-    document.getElementById("pswRepeat").style.border = '';
-  }
-
-  // Returns false if the password field is empty
-  if(!pwNotEmpty) {
     return false;
   }
 
-  // Returns false and sets the error message if the password and repeat password fields don't match
+  // Adds a message next to the repeat password field if the password and repeat password fields don't match
   if (pw !== pwRepeat) {
-    document.getElementById("message").innerHTML = "Passwords must match";
+    document.getElementById("repeatPassMessage").innerHTML = "Passwords must match";
+    passValid = false;
+  }
+
+  // Adds a message next to the password field if the password length is less then 8 characters
+  if (pw.length < 8) {
+    document.getElementById("passMessage").innerHTML = "Password must be at least 8 characters";
+    passValid = false;
+  }
+
+  // Returns false if the password input is invalid
+  if(!passValid) {
     return false;
   }
 
-  // Returns false if the password length is less then 8 characters
-  else if (pw.length < 8) {
-    document.getElementById("message").innerHTML = "Password must be at least 8 characters";
-    return false;
-  } 
-  
-  // Removes the error message if the password input is valid
+  // Returns true if the password input is valid
   else {
-    document.getElementById("message").innerHTML = "";
+    return true;
+  }
+}
+
+function validateEmail(email) {
+  document.getElementById("email").style.border = ''; // Resets the border style
+
+  // Checks if the email field is empty, sets the border color to red if it is
+  if(email === "") {
+    document.getElementById("email").style.border = '1px solid red';
+    return false; // Return false if the email field is empty
   }
 
-  return pwRepeatNotEmpty; // Returns true if the repeat password field isn't empty, false otherwise
+  return true; // Returns true if the email field isn't empty
 }
